d7b1d1e54169e91af6fd42042ca210de9dd966b2
GlebUrv <punkhoygleb@mail.ru>
2025-08-13 19:23:18 +0500

    catalog init

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Catalog.css";
+import Item from "../Item/Item";
+import { useTelegram } from "../../hooks/useTelegram";
+
+const products = [
+  { id: 1, title: "Product 1", description: "Description 1", price: 100 },
+  { id: 2, title: "Product 2", description: "Description 2", price: 200 },
+  { id: 3, title: "Product 3", description: "Description 3", price: 300 },
+];
+
+const getTotalPrice = (cart) => {
+  return cart.reduce((total, item) => total + item.price, 0);
+};
 
 const Catalog = () => {
-  return <div>CATALOG</div>;
+  const [cart, setCart] = useState([]);
+  const { tg } = useTelegram();
+
+  const onAdd = (product) => {
+    const alreadyInCart = cart.find((item) => item.id === product.id);
+    let newItems = [];
+
+    if (alreadyInCart) {
+      newItems = cart.filter((item) => item.id !== product.id);
+    } else {
+      newItems = [...cart, product];
+    }
+
+    setCart(newItems);
+
+    if (newItems.length === 0) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+      tg.MainButton.setParams({
+        text: `В корзине (${newItems.length}). Купить за ${getTotalPrice(
+          newItems
+        )} руб.`,
+      });
+    }
+  };
+
+  return (
+    <div className={"list"}>
+      {products.map((item) => (
+        <Item product={item} onAdd={onAdd} className={"item"} />
+      ))}
+    </div>
+  );
 };
 
 export default Catalog;
---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Item.css";
+import Button from "../Button/Button";
+
+const Item = ({ product, className, onAdd }) => {
+  const onAddHandler = () => {
+    onAdd(product);
+  };
+
+  return (
+    <div className={"product " + className}>
+      <div className={"img"} />
+      <div className={"title"}>{product.title}</div>
+      <div className={"description"}>{product.description}</div>
+      <div className={"price"}>Стоимость: {product.price}</div>
+      <Button className={"add-btn"} onClick={onAddHandler}>
+        Добавить в корзину
+      </Button>
+    </div>
+  );
+};
+
+export default Item;

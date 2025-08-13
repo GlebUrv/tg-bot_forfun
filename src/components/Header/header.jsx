b1dfef7e4d80a185a761bd1cc16a10e9447b9448
GlebUrv <punkhoygleb@mail.ru>
2025-08-13 19:26:48 +0500

    build fix

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./header.css";
-import Button from "../Button/button";
+import "./Header.css";
+import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
